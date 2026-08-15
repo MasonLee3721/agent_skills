be95fbbb9cb28448df6bb879ce71fd1211aa8a0d
MasonLee3721 <masonlee3721@gmail.com>
2026-08-15 15:07:00 +0000

    feat(kiro7): add run_joint.pl for joint foreign/sitc buy calculation

---
+++ kiro/kiro7/goodinfo-joint/run_joint.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use utf8;
+use JSON::PP;
+
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
+
+# 1. Fetch TWSE Capital
+my $json_cap = `curl -s "https://openapi.twse.com.tw/v1/opendata/t187ap03_L"`;
+my $cap_list = decode_json($json_cap);
+my %capital;
+for my $c (@$cap_list) {
+    my $code = $c->{"公司代號"};
+    my $cap = $c->{"實收資本額"};
+    $cap =~ s/,//g;
+    $capital{$code} = {
+        name => $c->{"公司簡稱"},
+        capital => $cap + 0,
+        shares => ($cap + 0) / 10,
+    };
+}
+
+# 2. Fetch TWSE T86
+my $json_t86 = `curl -s "https://www.twse.com.tw/rwd/zh/fund/T86?response=json&date=20260814&selectType=ALLBUT0999"`;
+my $t86_data = decode_json($json_t86);
+
+my @joint_list;
+for my $row (@{$t86_data->{data}}) {
+    my $code = $row->[0]; $code =~ s/\s+//g;
+    next unless exists $capital{$code};
+    
+    my $name = $row->[1]; $name =~ s/\s+//g;
+    my $foreign_buy = $row->[4]; $foreign_buy =~ s/,//g;
+    my $sitc_buy = $row->[10]; $sitc_buy =~ s/,//g;
+    my $total_buy = $row->[18]; $total_buy =~ s/,//g;
+    
+    my $shares = $capital{$code}->{shares};
+    next if $shares <= 0;
+    
+    my $sitc_pct = ($sitc_buy / $shares) * 100;
+    my $foreign_pct = ($foreign_buy / $shares) * 100;
+    my $total_pct = ($total_buy / $shares) * 100;
+    
+    if ($sitc_pct > 0.05 && $foreign_pct > 0.1) {
+        push @joint_list, {
+            code => $code,
+            name => $name,
+            shares_zhang => int($shares / 1000),
+            sitc_buy_zhang => int($sitc_buy / 1000),
+            sitc_pct => $sitc_pct,
+            foreign_buy_zhang => int($foreign_buy / 1000),
+            foreign_pct => $foreign_pct,
+            total_buy_zhang => int($total_buy / 1000),
+            total_pct => $total_pct,
+        };
+    }
+}
+
+@joint_list = sort { $b->{foreign_pct} <=> $a->{foreign_pct} } @joint_list;
+
+print "=== 📊 2026/08/14 上市股票 外資與投信法人同買強勢榜 ===\n";
+printf "%-4s | %-6s | %-10s | %-12s | %-12s | %-10s\n", "名次", "代號", "股票名稱", "投信買超(張/%)", "外資買超(張/%)", "法人合計%";
+print "-" x 70 . "\n";
+
+for my $i (0..9) {
+    last if $i >= @joint_list;
+    my $r = $joint_list[$i];
+    printf "%-4d | %-6s | %-10s | %d (+%.2f%%) | %d (+%.2f%%) | +%.2f%%\n",
+        $i+1, $r->{code}, $r->{name}, $r->{sitc_buy_zhang}, $r->{sitc_pct}, $r->{foreign_buy_zhang}, $r->{foreign_pct}, $r->{total_pct};
+}
